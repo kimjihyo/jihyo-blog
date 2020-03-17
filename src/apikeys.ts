ce59ebd09194c02f9ff847e6abf04141928fa914
Jihyo Kim <kimjihyo0325@gmail.com>
2020-03-16 20:12:12 -0700

    added apikeys for deployment

---
+++ src/apikeys.ts
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyCISvdufoHjKy0cmiCUVRHMUzUNoin_TE0',
+  authDomain: 'jihyo-blog.firebaseapp.com',
+  databaseURL: 'https://jihyo-blog.firebaseio.com',
+  projectId: 'jihyo-blog',
+  storageBucket: 'jihyo-blog.appspot.com',
+  messagingSenderId: '578973600649',
+  appId: '1:578973600649:web:4482976990f31f39d2ef58',
+  measurementId: 'G-M350N7G8RG',
+};
+
+export default firebaseConfig;
